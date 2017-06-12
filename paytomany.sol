d00cc4fbff925c364e944712998ded615c0015cd
mattdf <work@mattdf.com>
2017-06-12 12:31:26 +0100

    added events and refunds

--- paytomany.sol
+++ paytomany.sol
@@ -2,18 +2,39 @@ pragma solidity ^0.4.0;
 
 contract PayMany {
 
+	event PaymentSuccess(
+		address payee,
+		uint value
+	);
+
+	event PaymentFailure(
+		address payee,
+		uint value
+	);
 
 	function PayXToList(address[] list, uint value) public payable {
 
-		// if the value to send out is not exactly the amount of eth sent to the
-		// contract we know that somewhere in the loop the contract will fail
-		// do some overflow checks as well
-		if ((value * list.length) != msg.value || value < (value * list.length) || value > msg.value)
+		uint returnfunds = msg.value;
+		uint sentfunds = 0;
+
+		// do some overflow checks and bounds checks
+		if ((value * list.length) > msg.value || value < (value * list.length) || value > msg.value)
 			throw;
 
 		for (uint i = 0; i < list.length; i++){
-			if (!list[i].send(value))
-				throw;
+			if (!list[i].send(value)){
+				returnfunds -= value;
+				PaymentFailure(list[i], value);
+			}
+			else {
+				sentfunds += value;
+				PaymentSuccess(list[i], value);
+			}
+		}
+
+
+		if ((returnfunds + sentfunds != msg.value) || !msg.sender.send(returnfunds)){
+			throw;
 		}
 
 
@@ -24,25 +45,23 @@ contract PayMany {
 		uint sum = 0;
 		uint i;
 
-		// if there's not one value specified per address throw
-		if (list.length != values.length)
-			throw;
+		uint returnfunds = msg.value;
+		uint sentfunds = 0;
 
-		for (i = 0; i < values.length; i++){
-			sum += values[i];
-			// make sure we only try to send what we have, and some overflow checks
-			if (sum > msg.value || sum < values[i])
-				throw;
-		}
-
-		// if at the end of the tally the sum doesn't equal the msg.value, throw,
-		// else ETH will be stuck or likely the list was constructed wrong
-		if (sum != msg.value)
-			throw;
 
 		for (i = 0; i < list.length; i++){
-			if (!list[i].send(values[i]))
-				throw;
+			if (!list[i].send(values[i])){
+				returnfunds -= values[i];
+				PaymentFailure(list[i], values[i]);
+			}
+			else {
+				sentfunds += values[i];
+				PaymentSuccess(list[i], values[i]);
+			}
+		}
+
+		if ((returnfunds + sentfunds != msg.value) || !msg.sender.send(returnfunds)){
+			throw;
 		}
 
 	}
