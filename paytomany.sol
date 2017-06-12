dbb9282731f919c3939b099a6e5d302ae303ea5e
mattdf <work@mattdf.com>
2017-06-12 13:47:38 +0100

    update as per nick's comments

--- paytomany.sol
+++ paytomany.sol
@@ -14,55 +14,37 @@ contract PayMany {
 
 	function PayXToList(address[] list, uint value) public payable {
 
-		uint returnfunds = msg.value;
-		uint sentfunds = 0;
-
 		// do some overflow checks and bounds checks
 		if ((value * list.length) > msg.value || value < (value * list.length) || value > msg.value)
 			throw;
 
 		for (uint i = 0; i < list.length; i++){
 			if (!list[i].send(value)){
-				returnfunds -= value;
 				PaymentFailure(list[i], value);
 			}
 			else {
-				sentfunds += value;
 				PaymentSuccess(list[i], value);
 			}
 		}
 
 
-		if ((returnfunds + sentfunds != msg.value) || !msg.sender.send(returnfunds)){
-			throw;
-		}
-
+		msg.sender.transfer(this.balance);
 
 	}
 
 	function PayValsToList(address[] list, uint[] values) public payable {
 
-		uint sum = 0;
-		uint i;
-
-		uint returnfunds = msg.value;
-		uint sentfunds = 0;
-
-
-		for (i = 0; i < list.length; i++){
+		for (uint i = 0; i < list.length; i++){
 			if (!list[i].send(values[i])){
-				returnfunds -= values[i];
 				PaymentFailure(list[i], values[i]);
 			}
 			else {
-				sentfunds += values[i];
 				PaymentSuccess(list[i], values[i]);
 			}
 		}
 
-		if ((returnfunds + sentfunds != msg.value) || !msg.sender.send(returnfunds)){
-			throw;
-		}
+
+		msg.sender.transfer(this.balance);
 
 	}
 
